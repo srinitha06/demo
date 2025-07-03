350bb1889a873979ac8540b227c91ea8f287e24f
srinitha06 <srinitha.k2023ece@sece.ac.in>
2025-07-03 15:47:06 +0530

    DAY 2 AFTERNOON

--- demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -1,17 +1,30 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Student;
 import com.example.demo.services.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 @RestController
 
 public class HelloWorldController {
     @Autowired
     HelloWorldService hws;
     @GetMapping("/")
-    public String hello(){
-      return hws.helloWorld();
+    public List<Student> getMethod(){
+      return hws.getMethod();
     }
+    @PostMapping("/")
+    public String postMethod(){
+        return hws.postMethod();
+    }
+    @PutMapping("/")
+    public String putMethod(){
+        return hws.putMethod();
+    }
+    @DeleteMapping("/")
+    public String DeleteMethod(){
+        return hws.deleteMethod();
+    }
+
 }
--- demo/src/main/java/com/example/demo/services/HelloWorldService.java
+++ demo/src/main/java/com/example/demo/services/HelloWorldService.java
@@ -1,11 +1,33 @@
 package com.example.demo.services;
 
+import com.example.demo.models.Student;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class HelloWorldService {
-    public String helloWorld(){
-        return "HELLO WORLD";
+    List<Student> std=new ArrayList<>(
+            Arrays.asList(new Student(1,"sri","23ec159"),
+                    new Student(2,"nitha","23ec111"))
+    );
+
+    public List<Student> getMethod(){
+        return std;
+    }
+
+    public String postMethod(){
+        return"post method";
+    }
+
+    public String putMethod(){
+        return"put method";
+    }
+
+    public String deleteMethod(){
+        return"delete method";
     }
 }
 
