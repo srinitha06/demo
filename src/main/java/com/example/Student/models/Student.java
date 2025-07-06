4e63909e917d60a7761168cdf41a0a7991148ca7
srinitha06 <153302697+srinitha06@users.noreply.github.com>
2025-07-06 21:59:31 +0530

    Add files via upload
    day 3 student task

---
+++ src/main/java/com/example/Student/models/Student.java
@@ -0,0 +1,13 @@
+package com.example.Student.models;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private int stdID;
+    private String  std_name;
+    private String std_roll;
+
+}
