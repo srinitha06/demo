8de5255a5286cf98ef52b5ce59c168da12116239
srinitha06 <srinitha.k2023ece@sece.ac.in>
2025-07-15 15:44:33 +0530

    DAY 11 mrng

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,10 @@
 # Use Maven with JDK 21 to build
 FROM maven:3.9.6-eclipse-temurin-21 AS build
-
-WORKDIR /app
-
-COPY pom.xml .
-RUN mvn dependency:go-offline
-
-COPY src ./src
+COPY . .
 RUN mvn clean package -DskipTests
 
 # Use JDK 21 for runtime
 FROM eclipse-temurin:21-jdk
-
-WORKDIR /app
-COPY --from=build /app/target/*.jar demo.jar
-
+COPY --from=build /target/*.jar demo.jar
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "demo.jar"]
---
+++ demo/src/test/java/com/example/demo/controllers/EmployeeControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.RegisterDetails;
+import com.example.demo.services.EmployeeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class EmployeeControllerTest {
+    @Mock
+    EmployeeService employeeService;
+
+    @InjectMocks
+    EmployeeController employeeController;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testRoute(){
+        String result=employeeController.route();
+        assertEquals("Welcome to SpringBoot Security",result);
+
+    }
+
+    @Test
+    void testGetMethod(){
+        RegisterDetails emp1=new RegisterDetails();
+        RegisterDetails emp2=new RegisterDetails();
+        when(employeeService.getMethod()).thenReturn(Arrays.asList(emp1,emp2));
+        List<RegisterDetails> result=employeeController.getMethod();
+        assertEquals(2,result.size());
+    }
+}
